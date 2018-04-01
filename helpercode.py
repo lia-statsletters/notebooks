8862e495f3c8c41cab4c010f4ee0883b415e9d0a
Lia <lia.silva@gmail.com>
2018-04-01 19:26:37 +0200

    made naive method for generating booking data with uniformly distributed dates. length of booking can be controlled with a distribution from scipy stats

--- helpercode.py
+++ helpercode.py
@@ -1,20 +1,23 @@
 from scipy import stats as spst
 import numpy as np
+from numpy import timedelta64 as td64
 
-def generateData(distCallable,param,kwargsx,nsamples):
+
+def generateData(distCallable,param,kwargsx,nsamples,year='2017'):
     """distCallable is a distribution from scipy.stats,
     params is a parameter of the distribution,
     kwargsx is a set of keyword arguments,
     nsamples is the number of samples"""
-    try:
-        r = distCallable.rvs(param,size=nsamples,**kwargsx)
-    except Exception as whathappened:
-        print (whathappened.message)
-        return
-    startdays=np.random.random_integers(0,365,nsamples)
-    for sample in enumerate(r):
-        #generating dates in 2017.
-        delta=np.datetime64(sample,'s')
+
+    #ToDo: favour weekends, week 52, spring and summer on selecting starting dates.
+
+    # make all dates in a year (365)
+    allyear = np.arange('{}-01'.format(year), '{}-12'.format(year),dtype='datetime64[D]')
+    startdates=np.random.choice(allyear,size=nsamples) #select whatever stating dates uniformly
+    lengths=np.array(distCallable.rvs(param,size=nsamples,**kwargsx),dtype='timedelta64[s]') #lengths
+    return {'startdates':startdates,
+            'enddates':startdates+lengths,
+            'lengths':lengths}
 
 
 
