d7afbb2408b32f11664953cbf0debe375cae91db
Lia <lia.silva@gmail.com>
2018-04-07 20:33:35 +0200

    added a function that generates locations in degree coordinates given a center coordinate and a radius

--- helpercode.py
+++ helpercode.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import timedelta64 as td64
 
 
-def generateData(distCallable,param,kwargsx,nsamples,year='2017'):
+def generateTimeData(distCallable,param,kwargsx,nsamples,year='2017'):
     """distCallable is a distribution from scipy.stats,
     params is a parameter of the distribution,
     kwargsx is a set of keyword arguments for distCallable in scipy,
@@ -14,17 +14,45 @@ def generateData(distCallable,param,kwargsx,nsamples,year='2017'):
     # make all dates in a year (365)
     allyear = np.arange('{}-01'.format(year), '{}-12'.format(year),dtype='datetime64[D]')
     startdates=np.random.choice(allyear,size=nsamples) #select whatever stating dates uniformly
-    lengths=np.array(distCallable.rvs(param,size=nsamples,**kwargsx),dtype='timedelta64[s]') #lengths
+    lengths=np.array(distCallable.rvs(param,size=nsamples,**kwargsx),dtype='timedelta64[s]') #
     return {'startdates':startdates,
             'enddates':startdates+lengths,
             'lengths':lengths}
 
 
+def generateLocations(nlocations,rad,center,earthrad=6371.,param=3):
+    """Generate nlocations at max rad radius (in km) of a center.
+    center is given in {'lon': ,'lat':}.
+    param is the parameter of the powerlaw used to generate locations"""
+    #Generate radios and angles
+    radx = spst.powerlaw.rvs(param, size=nlocations) * rad
+    angles = spst.cosine.rvs(size=nlocations) # this dist makes angles in radians
+
+    #place points in cartesian
+    x=radx*np.cos(angles)
+    y=radx*np.sin(angles)
+
+    #centerpoint from coordinates to radians.
+    clat=np.radians(center['lat'])
+    #get center in cartesian
+    projlon=earthrad*np.sin(np.radians(center['lon']))
+    deltax=x+projlon*np.cos(clat)
+    deltay=y+projlon*np.sin(clat)
+
+    earthrad2,x2,y2=map(lambda squarables: np.power(squarables,2),
+                        [earthrad, deltax, deltay])
+
+    #return absolute long and lat of points
+    longs=np.arccos(np.sqrt( 1. - ( (x2+y2)/earthrad2) ) )
+    return {'lon': np.degrees(longs),
+            'lat': np.degrees(np.arccos( deltax/earthrad*np.sin(longs) ))}
+
 def haversine(p1, p2, earthrad=6371.):
 
     """
-    long and lat in decimal degrees.
-    The haversine formula determines the great-circle distance between two points on a
+    long and lat in decimal degrees, returns distance in km between points.
+    points given as dictionaries with keys 'lon' and 'lat'.
+    The haversine determines the great-circle distance between two points on a
     sphere given their longitudes and latitudes. Gives as-the-crow-flies distance between points.
     Check: https://en.wikipedia.org/wiki/Haversine_formula
     Based on: https://stackoverflow.com/questions/29545704/fast-haversine-approximation-python-pandas"""
@@ -44,8 +72,9 @@ def haversine(p1, p2, earthrad=6371.):
 def main():
     #generateData(spst.genpareto, 1.,
     #             {'loc':1800.,'scale':3.}, 10000)
-    print(haversine({'lat':57.7090,'lon':11.9745},{'lat': 57.75,'lon':11.975}) )
-
+    #print(haversine({'lat':57.7090,'lon':11.9745},{'lat': 57.75,'lon':11.975}) )
+    locations=generateLocations(100,10,{'lat':57.7090,'lon':11.9745})
+    print ("yay")
 
 if __name__ == "__main__":
     main()
