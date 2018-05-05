3d3d0c592dde3adc23871f7e5e62d6428b22b34b
Lia <lia.silva@gmail.com>
2018-05-05 19:55:11 +0200

    moved SQS tutorial into a python file. Using debugger to more easily poke the response fields.

---
+++ botodebugging.py
@@ -0,0 +1,53 @@
+import boto3
+import pprint
+
+def read_queue(queue_url,sqs_client):
+    """Barebones reader for everything in a queue."""
+    response = sqs_client.receive_message(QueueUrl=queue_url)
+    try:
+        print ('{} messages retreived.'.format(len(response['Messages'])))
+    except Exception as xx:
+        print ('No messages in queue, or {}'.format(xx))
+    return response
+
+def write_to_queue(message_attrs, message_body,
+                   queue_url, sqs_client, delay_secs=0):
+        """Put a message in a queue."""
+        response = sqs_client.send_message(
+            QueueUrl=queue_url,
+            DelaySeconds=delay_secs,
+            MessageAttributes=message_attrs,
+            MessageBody=message_body
+        )
+        try:
+            print('HTTPCode {} message successfully sent, '
+                  '{} retries'.format(response['ResponseMetadata']['HTTPStatusCode'],
+                                      response['ResponseMetadata']['RetryAttempts']))
+        except Exception as xx:
+            print('Problems sending the message: {}'.format(xx))
+
+        return response
+
+
+def main():
+    queue_url = 'https://eu-west-1.queue.amazonaws.com/901610709743/boto_tut_sqs'
+    sqs_client = boto3.client('sqs')
+    pp = pprint.PrettyPrinter(indent=4)
+
+    #Make some message to send to the queue
+    message_attrs={'Title': {'DataType': 'String',
+                             'StringValue': 'sometitle_tut'
+                            }
+                  }
+    message_body=('some_message_body_tut')
+
+    #Send the message to the queue
+    #confirmation=write_to_queue(message_attrs, message_body,
+    #               queue_url, sqs_client, delay_secs=0)
+    #pp.pprint(confirmation)
+
+    #Read the whole queue
+    pp.pprint(read_queue(queue_url,sqs_client))
+
+if __name__ == "__main__":
+    main()
