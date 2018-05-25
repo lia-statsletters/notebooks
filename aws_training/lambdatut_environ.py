12e5fe155d60ee72822f1c3baa6c9d9d5d06b462
Lia <lia.silva@combine.se>
2018-05-25 13:58:52 +0200

    adding some policy jsons

--- aws_training/lambdatut_environ.py
+++ aws_training/lambdatut_environ.py
@@ -1,10 +1,17 @@
 import os
 
-def my_handler(event, context):
+def handler(event, context):
     try:
-        print('Executing {} v.{}, weights {}MB in memory.'
+        print('Executing {} v.{}, weights {} MB in memory.'
               ''.format(os.environ['AWS_LAMBDA_FUNCTION_NAME'],
                         os.environ['AWS_LAMBDA_FUNCTION_VERSION'],
                         os.environ['AWS_LAMBDA_FUNCTION_MEMORY_SIZE']))
     except Exception as wtf:
         print(wtf.message)
+
+
+def main():
+    handler(0,0)
+
+if __name__ == "__main__":
+    main()
