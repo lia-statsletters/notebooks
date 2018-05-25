a426400eff43c247fb5f42427f88b00ea4251846
Lia <lia.silva@combine.se>
2018-05-25 10:50:07 +0200

    barebones example printing some Lambda environ vars

--- aws_training/lambdatut_environ.py
+++ aws_training/lambdatut_environ.py
@@ -1,6 +1,10 @@
 import os
 
 def my_handler(event, context):
-    foo=os.environ['foo']
-    print('foo={}'.format(foo))
-    return foo
+    try:
+        print('Executing {} v.{}, weights {}MB in memory.'
+              ''.format(os.environ['AWS_LAMBDA_FUNCTION_NAME'],
+                        os.environ['AWS_LAMBDA_FUNCTION_VERSION'],
+                        os.environ['AWS_LAMBDA_FUNCTION_MEMORY_SIZE']))
+    except Exception as wtf:
+        print(wtf.message)
